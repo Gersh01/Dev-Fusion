46af56b62c9fb64bdafa9a4f5b35e9262ef8142f
Alex Gershfeld <alex.gershfeld@gmail.com>
2024-06-20 20:39:03 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url);
+client.connect;
+
+//api-----------------
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT);
+})
+
