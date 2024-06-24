fcaa6fa4080a12c13af2e71c4eb45bc49286d862
Xutao Gao <taogeeee@gmail.com>
2024-06-23 21:12:15 -0400

    Add Reusable buttons, inputs, and refactored code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
+import AuthPage from "./pages/AuthPage";
 import LandingPage from "./pages/LandingPage";
-import SignInPage from "./pages/SignInPage";
-import RegisteringPage from "./pages/RegisteringPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+	// let theme = "";
+
+	// if (
+	// 	localStorage.theme === "dark" ||
+	// 	(!("theme" in localStorage) &&
+	// 		window.matchMedia("(prefers-color-scheme: dark)").matches)
+	// ) {
+	// 	document.documentElement.classList.add("dark");
+	// 	theme = "dark";
+	// } else {
+	// 	document.documentElement.classList.remove("dark");
+	// }
+
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<LandingPage />} />
-				<Route path="/login" element={<SignInPage />} />
-				<Route path="/register" element={<RegisteringPage />} />
+				<Route path="/login" element={<AuthPage />} />
+				<Route path="/signup" element={<AuthPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,45 +1,44 @@
 import { ReactTyped } from "react-typed";
+import Button from "../components/reusable/Button";
 
 const LandingPage = () => {
-  const goLogin = () => {
-    window.location.href = "/login";
-  };
+	const goToLogin = () => {
+		window.location.href = "/login";
+	};
 
-  return (
-    <div className="w-screen h-screen p-2.5 bg-gradient-to-br from-orange-300 to-orange-500 dark:bg-gradient-to-br dark:from-violet-600 dark:to-violet-700 justify-center items-center gap-2.5 inline-flex">
-      <div className="w-[400px] h-[520px] p-2.5 flex-col justify-start items-center gap-5 inline-flex">
-        <div className="self-stretch grow shrink basis-0 flex-col justify-center items-center gap-[20px] flex">
-          <div className="self-stretch h-[280px] flex-col justify-center items-center gap-4 flex">
-            <div className="self-stretch text-center text-white text-4xl font-bold italic inter uppercase leading-10">
-              Thrive in the power of open-source to drive innovation!
-            </div>
-            <div className="self-stretch text-center text-white text-md font-normal crimson-pro leading-tight">
-              Whether you are a seasoned developer or just starting out, you
-              will find a place here.
-            </div>
-            <div className="self-stretch text-center text-white text-md font-normal crimson-pro leading-tight">
-              connect with like-minded individuals who share your passion for
-              coding.
-            </div>
-          </div>
-          <div className="self-stretch text-center text-white text-2xl font-semibold inter leading-tight">
-            <ReactTyped
-              strings={[
-                "Together, we can build a brighter future, one line of code at a time!",
-              ]}
-              typeSpeed={60}
-            />
-          </div>
-        </div>
-        <button
-          className="self-stretch p-2.5 bg-white rounded-[5px] justify-center items-center gap-2.5 inline-flex text-black text-xl font-semibold font-['League Spartan']"
-          onClick={goLogin}
-        >
-          Get Started
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="w-screen h-screen p-2.5 bg-gradient-to-br from-orange-300 to-orange-500 
+			dark:bg-gradient-to-br dark:from-violet-600 dark:to-violet-700 justify-center items-center gap-3 inline-flex"
+		>
+			<div className="w-[400px] p-2 flex flex-col justify-start gap-20">
+				<div className="flex flex-col gap-5">
+					<div className="text-center text-white text-4xl font-bold italic inter uppercase leading-10">
+						Thrive in the power of open-source to drive innovation!
+					</div>
+					<div className="text-center text-white text-md font-normal crimson-pro leading-tight">
+						Whether you are a seasoned developer or just starting
+						out, you will find a place here.
+					</div>
+					<div className="text-center text-white text-md font-normal crimson-pro leading-tight">
+						connect with like-minded individuals who share your
+						passion for coding.
+					</div>
+					<div className="text-center text-white text-2xl font-semibold inter leading-tight">
+						<ReactTyped
+							strings={[
+								"Together, we can build a brighter future, one line of code at a time!",
+							]}
+							typeSpeed={40}
+						/>
+					</div>
+				</div>
+				<Button large mode="white" onClick={goToLogin}>
+					Get Started
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default LandingPage;
