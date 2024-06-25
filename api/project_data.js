81c062eca856d5419b058313461c5acbfbde5384
GoldenLin9 <goldenlin0909@gmail.com>
2024-06-25 19:26:14 -0400

    Create Project API w/ YAML documentation

--- api/project_data.js
+++ api/project_data.js
@@ -19,4 +19,43 @@ const cookieJwtAuth = (req, res, next) => {
 
 exports.setApp = function (app, client) {
     
+    // create a project
+    app.post('/api/project', async (req, res, next) => {
+        let isOpen = Boolean(req.body.isOpen);
+        let isDone = Boolean(req.body.isDone);
+        let isStarted = Boolean(req.body.isStarted);
+        let dateCreated = new Date(req.body.dateCreated);
+        let ownerID = new ObjectId(req.body.ownerID);
+        let currentVsRequired = req.body.currentVsRequired;
+        let deadline = new Date(req.body.deadline);
+        let projectStartDate = new Date(req.body.projectStartDate);
+        let roles = req.body.roles;
+        let technologies = req.body.technologies;
+        let title = req.body.title;
+
+        try {
+            db = client.db("DevFusion");
+            project = {
+                isOpen: isOpen,
+                isDone: isDone,
+                isStarted: isStarted,
+                dateCreated: dateCreated,
+                ownerID: ownerID,
+                currentVsRequired: currentVsRequired,
+                deadline: deadline,
+                projectStartDate: projectStartDate,
+                roles: roles,
+                technologies: technologies,
+                title: title
+            };
+
+
+            db.collection("ProjectData").insertOne(project);
+            return res.sendStatus(200);
+        } catch (e) {
+            error = e.toString();
+            let ret = {error: error};
+            return res.status(500).json(ret);
+        }
+    })
 }
