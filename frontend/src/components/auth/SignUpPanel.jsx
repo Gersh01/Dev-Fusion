5f28f850b06a4c6c89833eb83028de014407c757
James Salzer <salzer.james@yahoo.com>
2024-06-24 22:10:09 -0400

    Added Authentication fields
    - Added field checks

--- frontend/src/components/auth/SignUpPanel.jsx
+++ frontend/src/components/auth/SignUpPanel.jsx
@@ -3,49 +3,111 @@ import Input from "../reusable/Input";
 import { MdAccountCircle, MdLockOpen, MdEmail } from "react-icons/md";
 import Button from "../reusable/Button";
 import AuthPanel from "../reusable/AuthPanel";
+import { useState } from "react";
+import PasswordChecklist from "react-password-checklist";
 
 const SignUpPanel = () => {
-	const goToLogin = () => {
-		window.location.href = "/login";
-	};
+  const validEmail = new RegExp(
+    "^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"
+  );
 
-	return (
-		<AuthPanel width={480} minHeight={750}>
-			<AuthHeader title="Sign Up" />
-			<div className="flex flex-col gap-2">
-				<div className="grid grid-cols-2 gap-2 min-w-0">
-					<Input titleText="First Name" placeholder="First Name" />
-					<Input titleText="Last Name" placeholder="Last Name" />
-				</div>
-				<Input
-					titleText="Username"
-					placeholder="Username"
-					icon={<MdAccountCircle />}
-				/>
-				<Input
-					titleText="Email"
-					placeholder="Email"
-					icon={<MdEmail />}
-				/>
-				<Input
-					titleText="Password"
-					placeholder="Password"
-					icon={<MdLockOpen />}
-					password
-				/>
-			</div>
-			<div className="flex flex-col gap-3">
-				<Button large>Sign Up</Button>
-				<hr></hr>
-				<button
-					className="text-black dark:text-white text-sm font-medium poppins"
-					onClick={goToLogin}
-				>
-					Login instead
-				</button>
-			</div>
-		</AuthPanel>
-	);
+  const [fisrtName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const addError = (error) => {
+    const currentErrors = [...errors, error];
+    const listErrors = Array.from(new Set(currentErrors));
+    setErrors(listErrors);
+  };
+
+  const renderError = errors.map((error) => {
+    console.log(errors);
+    return <li key={error}>{error}</li>;
+  });
+
+  const doRegister = async (e) => {
+    if (
+      fisrtName !== "" &&
+      lastName !== "" &&
+      username !== "" &&
+      password !== "" &&
+      email !== ""
+    ) {
+      setErrors([]);
+      if (validEmail.test(email)) {
+        console.log("Password is correct: " + password);
+      }
+    } else {
+      addError("Field is missing");
+    }
+  };
+
+  const goToLogin = () => {
+    window.location.href = "/login";
+  };
+
+  return (
+    <AuthPanel width={480} minHeight={750}>
+      <AuthHeader title="Sign Up" />
+      <div className="flex flex-col gap-2">
+        <div className="grid grid-cols-2 gap-2 min-w-0">
+          <Input
+            titleText="First Name"
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <Input
+            titleText="Last Name"
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <Input
+          titleText="Username"
+          placeholder="Username"
+          icon={<MdAccountCircle />}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          titleText="Email"
+          placeholder="Email"
+          icon={<MdEmail />}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          titleText="Password"
+          placeholder="Password"
+          icon={<MdLockOpen />}
+          password
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="h-30flex flex-col grow text-white ">
+        <ul>{renderError}</ul>
+        <PasswordChecklist
+          rules={["capital", "specialChar", "minLength", "number"]}
+          minLength={8}
+          value={password}
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <Button large onClick={doRegister}>
+          Sign Up
+        </Button>
+        <hr></hr>
+        <button
+          className="text-black dark:text-white text-sm font-medium poppins"
+          onClick={goToLogin}
+        >
+          Login instead
+        </button>
+      </div>
+    </AuthPanel>
+  );
 };
 
 export default SignUpPanel;
