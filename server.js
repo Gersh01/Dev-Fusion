4bdb566da8961cf497624b7a519710038cec2e02
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2024-06-25 19:13:24 -0400

    resend_email_verification api done

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ require("dotenv").config();
 
 const appName = "https://dev-fusion-3adc28f56db6.herokuapp.com/";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.set("port", process.env.PORT || 5000);
+app.set("port", PORT);
 
 app.use(cors());
 app.use(bodyParser.json());
