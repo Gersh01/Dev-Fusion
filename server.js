3069feb6245afd9588c4852f1da08fa113fa5454
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2024-06-26 15:02:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
 	res.setHeader(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
