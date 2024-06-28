e2acd078facba2a84549cf983eefee6713feb3a4
James Salzer <salzer.james@yahoo.com>
2024-06-28 11:30:41 -0400

    Updated

--- frontend/src/pages/loaders/userSelector.js
+++ frontend/src/pages/loaders/userSelector.js
@@ -1,12 +1,9 @@
 import { useSelector,useDispatch } from "react-redux"
-import { addUser } from "../../store/slices/userSlice";
+
 
 export const userSelector = () => {
-
-
     let auth = useSelector((state)=>state.user.value) 
     return auth
-
 }
 
 
