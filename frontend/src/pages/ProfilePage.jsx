ffa65bc03fde165f88ea8725bd44f38712a246b7
Xutao Gao <taogeeee@gmail.com>
2024-06-28 11:57:12 -0400

    Refactor loginPage state code and removed redundant code

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -3,28 +3,28 @@ import { Fragment } from "react";
 import { useSelector } from "react-redux";
 
 const ProfilePage = () => {
-  let res = useSelector((state) => state.user.value.username);
+	let res = useSelector((state) => state.user);
 
-  return (
-    <Fragment>
-      <div
-        className="flex justify-between items-end flex-wrap gap-y-2 
+	return (
+		<Fragment>
+			<div
+				className="flex justify-between items-end flex-wrap gap-y-2 
 					text-black dark:text-white poppins text-4xl font-bold gap-x-6"
-      >
-        <p>Profile</p>
-      </div>
-      <Divider />
-      <div className="bg-white text-3xl font-bold poppins text-black">
-        {res}
-      </div>
-      <div className="flex flex-row gap-10">
-        <div className="bg-white h-[300px] w-[300px]"></div>
-        <div className="bg-white h-[300px] w-[300px]"></div>
-      </div>
-      <div className="grid md:grid-cols-2 gap-x-6 gap-y-6"></div>
-      <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-8"></div>
-    </Fragment>
-  );
+			>
+				<p>Profile</p>
+			</div>
+			<Divider />
+			<div className="bg-white text-3xl font-bold poppins text-black">
+				{res.username}
+			</div>
+			<div className="flex flex-row gap-10">
+				<div className="bg-white h-[300px] w-[300px]"></div>
+				<div className="bg-white h-[300px] w-[300px]"></div>
+			</div>
+			<div className="grid md:grid-cols-2 gap-x-6 gap-y-6"></div>
+			<div className="grid md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-8"></div>
+		</Fragment>
+	);
 };
 
 export default ProfilePage;
--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -1,30 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getUserFromJwt } from "../../pages/loaders/userLoader";
 
 const userSlice = createSlice({
 	name: "user",
-	initialState: {
-		// value: getUserFromJwt(),
-		value:{
-			"id": "667147c37d5f8d94a8a374c1",
-			"firstName": "Golden",
-			"lastName": "Lin",
-			"email": "goldenlin0909@gmail.com",
-			"username": "GoldenL",
-			"bio": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel atque aspernatur saepe praesentium minus, a distinctio dolor voluptates delectus?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel atque aspernatur saepe praesentium minus, a distinctio dolor voluptates delectus?",
-			"technologies": [
-				"VsCode"
-			],
-			"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkdvbGRlbkwiLCJpYXQiOjE3MTk1ODc1NTcsImV4cCI6MTcxOTY3Mzk1N30.-mnunFXAP0lZI9lq_OvbB0hCvKnbHakIgdXUg62JhRk",
-			"error": ""
-		}
-	},
+	initialState: {},
 	reducers: {
-		addUser:(state,action)=>{
-			state.value = action.payload;
-		}
+		setUser: (_, action) => {
+			return action.payload;
+		},
 	},
 });
-export const {addUser} = userSlice.actions
+export const { setUser } = userSlice.actions;
 
 export default userSlice.reducer;
