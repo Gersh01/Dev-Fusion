5c203da860751990b570810e9cb3508c02e02fca
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2024-06-28 19:14:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === "production") {
 
 	app.get("*", (req, res) => {
 		res.sendFile(
-			path.resolve(__dirname, "frontend", "build", "index.html")
+			path.resolve(__dirname, "frontend", "index.html")
 		);
 	});
 }
