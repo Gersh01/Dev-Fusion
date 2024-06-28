9bc43adf87d657c91f173e9a46f3e48a6aee70dc
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2024-06-27 23:41:54 -0400

    reject_member api done

--- api/inbox.js
+++ api/inbox.js
@@ -21,7 +21,8 @@ exports.setApp = function (app, client) {
 
     //api for getting all members who applied
     app.get('/api/inbox', async (req, res, next) => {
-        const projectId = req.body.projectId;
+        const projectId = req.body.projectId || "";
+        if(projectId == "") return res.status(400).json({error: "projectId is empty"});
         const nid = new ObjectId(projectId);
         var error = "";
 
@@ -62,8 +63,40 @@ exports.setApp = function (app, client) {
     });
 
     //api for rejecting an application
-    app.post('/api/inbox/reject_member', async (req, res, next) => {
+    app.delete('/api/inbox/reject_member', async (req, res, next) => {
+        const projectId = req.body.projectId || "";
+        const userId = req.body.userId || "";
+        const role = req.body.role || "";
+        if(projectId == "") return res.status(400).json({error: "projectId is empty"});
+        if(userId == "") return res.status(400).json({error: "userId is empty"});
+
+        var error = "";
+        const projectNid = new ObjectId(projectId);
+        const userNid = new ObjectId(userId);
         
+        var db;
+        var resultsFind;
+        var resultsDelete;
+        try {
+            db = client.db('DevFusion');
+            if(role == "") resultsFind = await db.collection('Inbox').find({ projectID: projectNid, userID: userNid }).toArray();
+            else resultsFind = await db.collection('Inbox').find({ projectID: projectNid, userID: userNid, role: role }).toArray();
+            if(resultsFind.length > 0){
+                if(role == "") resultsDelete = await db.collection('Inbox').deleteMany({projectID: projectNid, userID: userNid});
+                else resultsDelete = await db.collection('Inbox').deleteMany({projectID: projectNid, userID: userNid, role: role});
+                var ret = { error: error };
+                return res.status(200).json(ret);
+            }else{
+                error = "No applications found";
+                var ret = { error: error };
+                return res.status(404).json(ret);
+            }
+        } catch (e) {
+            error = e.toString;
+            var ret = { error: error };
+            return res.status(500).json(ret);
+        }
+
     });
 
 }
