12d12a11b8e5f1cd89b0b454a381416938e51b7d
Xutao Gao <taogeeee@gmail.com>
2024-06-29 19:09:34 -0400

    Connect view project API to view project page

--- frontend/src/pages/ViewProjectPage.jsx
+++ frontend/src/pages/ViewProjectPage.jsx
@@ -9,19 +9,71 @@ import Button from "../components/reusable/Button";
 import Bubble from "../components/reusable/Bubble";
 import ContentBubble from "../components/reusable/ContentBubble";
 import UserBubble from "../components/reusable/UserBubble";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const ViewProjectPage = () => {
+	const projectData = useLoaderData();
+	const navigate = useNavigate();
+
+	if (projectData === null) {
+		return null;
+	}
+
 	const numTotalPositions = 9;
-	const numDaysTilStart = 7;
+
+	const {
+		title,
+		description,
+		technologies,
+		communications,
+		projectStartDate,
+		deadline,
+	} = projectData;
+
+	const numDaysTilStart = Math.floor(
+		(new Date(projectStartDate) - new Date()) / 1000 / 60 / 60 / 24 + 1
+	);
+	const numDaysTilEnd = Math.floor(
+		(new Date(deadline) - new Date()) / 1000 / 60 / 60 / 24 + 1
+	);
+
+	let dateMessage = "";
+
+	if (numDaysTilStart > 0) {
+		if (numDaysTilStart === 1) {
+			dateMessage = `${numDaysTilStart} day until project begins`;
+		} else {
+			dateMessage = `${numDaysTilStart} days until project begins`;
+		}
+	} else {
+		if (numDaysTilEnd === 1) {
+			dateMessage = `due in ${numDaysTilEnd} day`;
+		} else {
+			dateMessage = `due in ${numDaysTilEnd} days`;
+		}
+	}
+
+	const renderedTechnologyBubbles = technologies?.map((technology) => {
+		return <Bubble key={technology} text={technology} />;
+	});
+
+	const renderedCommunicationBubble = communications?.map((communication) => {
+		return <Bubble key={communication} text={communication} />;
+	});
 
 	return (
 		<Fragment>
-			<button className="flex gap-2 items-center self-start">
+			<button
+				className="flex items-center self-start"
+				onClick={() => {
+					navigate(-1);
+				}}
+			>
 				<MdArrowLeft className="text-2xl" />
 				<p className="text-xl font-semibold">Back</p>
 			</button>
 			{/* <Divider /> */}
-			<p className="text-3xl font-semibold">Project Title</p>
+			<p className="text-3xl font-semibold">{title}</p>
 			<div className="flex gap-2 flex-wrap">
 				<Button mode="safe">Begin</Button>
 				<Button mode="secondary">Manage Team</Button>
@@ -36,59 +88,14 @@ const ViewProjectPage = () => {
 				</div>
 				<div className="flex gap-1 items-center">
 					<MdOutlineAccessTimeFilled className="text-xl text-white" />
-					<p className="poppins text-white">
-						{numDaysTilStart}{" "}
-						{numDaysTilStart === 1
-							? "day left to join"
-							: "days left to join"}
-					</p>
+					<p className="poppins text-white">{dateMessage}</p>
 				</div>
 			</div>
 			<Divider />
 			{/* DESCRIPTION */}
 			<div className="flex flex-col gap-2">
 				<p className="text-xl font-semibold">Description</p>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-					corporis minima illo corrupti rem nesciunt temporibus?
-					Deleniti corporis voluptatibus, doloremque fuga quasi,
-					eaque, dicta aut perferendis id iusto error consectetur
-					repudiandae! Cumque quam dicta laboriosam repellendus quia?
-					Deleniti natus, dolores at error laboriosam consequuntur
-					ratione quae, sed modi incidunt tempora aspernatur animi
-					fugit quod! Earum facilis illum ipsa itaque eum corrupti,
-					error inventore accusantium.
-				</p>
-				<p>
-					Doloremque magni delectus est? Dolorem, saepe. At sit a,
-					exercitationem praesentium dolor vero dolorem sed minus,
-					expedita mollitia possimus adipisci optio enim ad vel fugit
-					accusantium? Neque soluta inventore, veritatis ratione
-					labore accusamus nulla a reiciendis voluptas id blanditiis,
-					consequuntur ipsam quam sunt odio? Voluptate repudiandae hic
-					cumque eveniet laudantium, animi non sequi quibusdam
-					obcaecati, accusantium adipisci omnis beatae maiores fugiat
-					laborum ab eligendi ipsum impedit! Beatae facilis
-					accusantium, harum quo quae vero facere culpa. Quo
-					aspernatur labore molestias nostrum fugiat quos ad eligendi
-					aliquid? Ullam minima veritatis dignissimos minus ex iure
-					omnis, voluptatum quo, aliquid possimus aliquam repellendus
-					necessitatibus dolorem consectetur deleniti ratione eligendi
-					voluptates cupiditate eveniet placeat. Eius vero vitae iure
-					nisi quia nesciunt minima vel quos?
-				</p>
-				<p>
-					Libero, molestias. Inventore eum quidem placeat unde
-					exercitationem, vitae sunt eos alias repudiandae amet
-					incidunt, temporibus eligendi laboriosam! Rerum ex possimus
-					officia. Distinctio et nulla odio quam. Fugiat aliquam harum
-					nostrum, temporibus magni suscipit ducimus maxime provident
-					nobis cum, omnis incidunt? Inventore pariatur odio
-					reiciendis ratione repellendus numquam, ipsum ea provident
-					vitae voluptas illo eius laboriosam dolorem facere maxime!
-					Officia, illo consequatur debitis inventore rerum
-					repellendus explicabo.
-				</p>
+				<p>{description}</p>
 			</div>
 			{/* POSITIONS NEEDED */}
 			<div className="flex flex-col gap-2">
@@ -103,56 +110,24 @@ const ViewProjectPage = () => {
 			<div className="flex flex-col gap-2">
 				<p className="text-xl font-semibold">Position Requirements</p>
 				<div className="flex flex-col gap-4">
-					<ContentBubble text="Project Manager">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Quod aspernatur accusantium sit architecto recusandae id
-						iusto eum, adipisci atque assumenda.
-					</ContentBubble>
-					<ContentBubble text="Frontend">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Aliquam nemo asperiores eum quasi. Reprehenderit,
-						laboriosam.
-					</ContentBubble>
-					<ContentBubble text="API">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Labore dolorem possimus voluptas officiis repudiandae
-						veritatis amet corrupti quisquam?
-					</ContentBubble>
-					<ContentBubble text="Database">
-						Lorem ipsum, dolor sit amet consectetur adipisicing
-						elit. Exercitationem nostrum, perspiciatis sit veritatis
-						vero vitae quidem neque labore molestias odio asperiores
-						delectus iste dolorum commodi unde voluptatibus
-						similique, maiores corporis consequatur eius.
-					</ContentBubble>
+					<ContentBubble text="Project Manager"></ContentBubble>
+					<ContentBubble text="Frontend"></ContentBubble>
+					<ContentBubble text="API"></ContentBubble>
+					<ContentBubble text="Database"></ContentBubble>
 				</div>
 			</div>
 			{/* TECHNOLOGIES */}
 			<div className="flex flex-col gap-2">
 				<p className="text-xl font-semibold">Technologies</p>
 				<div className="flex flex-wrap gap-2">
-					<Bubble text="React" />
-					<Bubble text="Express" />
-					<Bubble text="MongoDB" />
-					<Bubble text="NodeJs" />
-					<Bubble text="Heroku" />
-					<Bubble text="Python" />
-					<Bubble text="ChatGPT" />
-					<Bubble text="Git" />
-					<Bubble text="GitHub" />
-					<Bubble text="TypeScript" />
-					<Bubble text="JavaScript" />
-					<Bubble text="Redux" />
+					{renderedTechnologyBubbles}
 				</div>
 			</div>
 			{/* COMMUICATION */}
 			<div className="flex flex-col gap-2">
 				<p className="text-xl font-semibold">Communications</p>
 				<div className="flex flex-wrap gap-2">
-					<Bubble text="iMessage" />
-					<Bubble text="Discord" />
-					<Bubble text="GitHub" />
-					<Bubble text="WhatsApp" />
+					{renderedCommunicationBubble}
 				</div>
 			</div>
 			{/* CURRENT TEAM */}
--- frontend/src/pages/loaders/projectLoader.js
+++ frontend/src/pages/loaders/projectLoader.js
@@ -13,4 +13,16 @@ const getProjects = async (queryConfig) => {
 	return response.data;
 };
 
-export { getProjects };
+const getProjectById = async (projectId) => {
+	console.log("Debug: Fetching project with ID: " + projectId);
+
+	const response = await axios.get(
+		`http://localhost:5000/api/project/${projectId}`,
+		{},
+		{ withCredentials: true }
+	);
+
+	return response.data;
+};
+
+export { getProjects, getProjectById };
