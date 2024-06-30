f6daac680ad910324ca194b2e338563866061ec5
Xutao Gao <taogeeee@gmail.com>
2024-06-30 14:26:20 -0400

    Fix route error where /reset-password-email and /reset-password cannot be reached

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,80 +1,64 @@
 import Bubble from "../components/reusable/Bubble";
 import Input from "../components/reusable/Input";
 import Button from "../components/reusable/Button";
-import NavBar from "../components/nav/NavBar"
+import NavBar from "../components/nav/NavBar";
 import BubblePanel from "../components/reusable/BubblePanel";
 
+const CreatePage = () => {
+	//TODO: Finish Creation Function
+	const doCreate = async (e) => {};
 
-const CreatePage = () =>
-    {
-        
-        //TODO: Finish Creation Function
-        const doCreate = async (e) => {
+	return (
+		<div>
+			<NavBar />
+			<div className="">
+				{/* Project Title */}
+				<Input
+					titleText="Project Title"
+					placeHolder="Enter Project Title"
+				></Input>
 
-        }
+				{/* TODO: Look into different format for date input */}
+				<div className="flex gap-2">
+					{/* Start Date */}
+					<Input
+						titleText="Start Date"
+						placeHolder="Enter the start date"
+					></Input>
 
-        return(
-            <div>
-                <NavBar />
-                <div className="">
-                    
-                    {/* Project Title */}
-                    <Input
-                    titleText="Project Title"
-                    placeHolder="Enter Project Title">
-                    </Input>
+					{/* Deadline */}
+					<Input
+						titleText="Deadline"
+						placeHolder="Enter the deadline"
+					></Input>
+				</div>
 
-                    {/* TODO: Look into different format for date input */}
-                    <div className="flex gap-2">
+				{/* Description */}
+				<Input
+					titleText="Description"
+					placeHolder="Tell us all about your project"
+				></Input>
 
-                        {/* Start Date */}
-                        <Input
-                        titleText="Start Date"
-                        placeHolder="Enter the start date">
-                        </Input>
-                    
-                        {/* Deadline */}
-                        <Input
-                        titleText="Deadline"
-                        placeHolder="Enter the deadline">
-                        </Input>
-                    </div>
+				{/* TODO: Technologies */}
+				<BubblePanel titleText="Technologies" techType></BubblePanel>
 
-                    {/* Description */}
-                    <Input
-                    titleText="Description"
-                    placeHolder="Tell us all about your project">
-                    </Input>
+				{/* Expectations */}
+				<Input
+					titleText="Expectations"
+					placeHolder="What do you need from your roles?"
+				></Input>
 
-                    {/* TODO: Technologies */}
-                    <BubblePanel
-                    titleText="Technologies"
-                    techType>
-                    </BubblePanel>
-                    
-                    {/* Expectations */}
-                    <Input
-                    titleText="Expectations"
-                    placeHolder="What do you need from your roles?">
-                    </Input>
+				{/* TODO: Roles */}
+				<BubblePanel titleText="Roles" roleType></BubblePanel>
 
-                    {/* TODO: Roles */}
-                    <BubblePanel
-                    titleText="Roles"
-                    roleType>
-                    </BubblePanel>
+				{/* TODO: Communication */}
+				<BubblePanel titleText="Communication" commsType></BubblePanel>
 
-                    {/* TODO: Communication */}
-                    <BubblePanel
-                    titleText="Communication"
-                    commsType>
-                    </BubblePanel>
-
-                    {/* Publish */}
-                    <Button onClick={doCreate} large>
-                        Publish
-                    </Button>
-                </div>
-            </div>
-        );
-    };
+				{/* Publish */}
+				<Button onClick={doCreate} large>
+					Publish
+				</Button>
+			</div>
+		</div>
+	);
+};
--- frontend/src/pages/ResetAuthPage.jsx
+++ frontend/src/pages/ResetAuthPage.jsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import AuthLayout from "../components/auth/AuthLayout";
 
 const ResetAuthPage = () => {
+	console.log("YEEHAH");
+
 	return (
 		<AuthLayout>
 			<Outlet />
