85c19699a973289179f2911d0b8541c96a8ded80
James Salzer <salzer.james@yahoo.com>
2024-06-30 21:30:19 -0400

    Added functionality to the resetpasswordpage

--- frontend/src/components/reusable/AuthPanel.jsx
+++ frontend/src/components/reusable/AuthPanel.jsx
@@ -5,24 +5,24 @@ import { twMerge } from "tailwind-merge";
 // * minHeight: number -> minimum height of the panel
 // * transparent: boolean -> whether to set panel to transparent background
 const AuthPanel = ({ children, width, minHeight, transparent }) => {
-	const widthStyle = `w-[${width || 480}px]`;
-	// const minHeightStyle = minHeight ? `min-h-[${minHeight}px]` : `min-h-fit`;
-	const minHeightStyle = "";
+  const widthStyle = `w-[${width || 480}px]`;
+  // const minHeightStyle = minHeight ? `min-h-[${minHeight}px]` : `min-h-fit`;
+  const minHeightStyle = "";
 
-	const panelColorStyle = twMerge(
-		classNames("bg-gray-100 dark:bg-gray-800", {
-			"bg-transparent dark:bg-transparent": transparent,
-		})
-	);
+  const panelColorStyle = twMerge(
+    classNames("bg-gray-100 dark:bg-gray-800", {
+      "bg-transparent dark:bg-transparent": transparent,
+    })
+  );
 
-	return (
-		<div
-			className={`py-24 ${widthStyle} sm:${minHeightStyle} ${panelColorStyle} 
+  return (
+    <div
+      className={`py-24 ${widthStyle} sm:${minHeightStyle} ${panelColorStyle} 
 			px-4 sm:px-7 rounded-3xl flex flex-col justify-center gap-12`}
-		>
-			{children}
-		</div>
-	);
+    >
+      {children}
+    </div>
+  );
 };
 
 export default AuthPanel;
--- frontend/src/pages/ResetPasswordPage.jsx
+++ frontend/src/pages/ResetPasswordPage.jsx
@@ -1,19 +1,36 @@
-// import ResetPasswordFields from "./ResetPasswordFields";
-// import ResetPasswordButton from "./ResetPasswordButton";
 import AuthPanel from "../components/reusable/AuthPanel";
+import { useState } from "react";
 import Input from "../components/reusable/Input";
 import { MdLockOpen } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import PasswordChecklist from "react-password-checklist";
+import Button from "../components/reusable/Button";
 
 const ResetPasswordPage = () => {
   const navigate = useNavigate();
+  const validPassword = new RegExp(
+    "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&])(?=.{8,})"
+  );
+  const [errorMessage, setErrorMessage] = useState("");
+  const [passwordField, setPasswordField] = useState(false);
+
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const showPasswordField = () => {
+    setPasswordField(true);
+  };
+  const hidePasswordField = () => {
+    setPasswordField(false);
+  };
+
   const goLogin = () => {
-    navigate("/login");
+    // navigate("/login");
   };
 
   return (
     // <div className="w-[450px] h-[500px] p-7 bg-gray-100 dark:bg-gray-800 rounded-3xl flex flex-col justify-center gap-12">
-    <AuthPanel width={480} minHeight={500}>
+    <AuthPanel width={480} minHeight={600} passwordFieldHeight={800}>
       <p className="text-center text-2xl font-semibold league-spartan dark:text-white">
         Reset Password
       </p>
@@ -23,26 +40,32 @@ const ResetPasswordPage = () => {
           placeholder="Password"
           icon={<MdLockOpen />}
           password
+          onChange={(e) => setPassword(e.target.value)}
+          onFocus={showPasswordField}
+          onBlur={hidePasswordField}
         />
         <Input
           titleText="Confirm Password"
           placeholder="Confirm Password"
           icon={<MdLockOpen />}
           password
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          onFocus={showPasswordField}
+          onBlur={hidePasswordField}
         />
       </div>
       <div className="self-stretch h-[78px] flex-col justify-center items-center gap-2.5 flex">
-        <button
-          className="self-stretch p-2.5 bg-gradient-to-r from-orange-400 to-orange-500 
-	  			dark:bg-gradient-to-br dark:from-violet-600 dark:to-violet-700 rounded-[5px] justify-center items-center gap-2.5 inline-flex"
-        >
-          <div
-            className="text-white text-xl font-semibold league-spartan"
-            onClick={goLogin}
-          >
-            Reset Password
-          </div>
-        </button>
+        <Button large>Reset Password</Button>
+        <div className=" h-[100px] bg-white flex flex-col grow dark:text-white text-sm">
+          {passwordField && (
+            <PasswordChecklist
+              className="poppins"
+              rules={["capital", "specialChar", "minLength", "number"]}
+              minLength={8}
+              value={password}
+            />
+          )}
+        </div>
       </div>
     </AuthPanel>
   );
