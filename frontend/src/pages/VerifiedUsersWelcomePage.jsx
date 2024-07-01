12e9b98718ba6d84e93527a929ab78e017e09676
James Salzer <salzer.james@yahoo.com>
2024-07-01 17:33:21 -0400

    Updated page styling to be more accurate with other pages

--- frontend/src/pages/VerifiedUsersWelcomePage.jsx
+++ frontend/src/pages/VerifiedUsersWelcomePage.jsx
@@ -9,23 +9,21 @@ const VerifiedUsersWelcomePage = () => {
 
   return (
     <AuthPanel width={480} minHeight={600}>
-      <p className="text-black text-3xl font-semibold dark:text-white league-spartan text-center">
-        Welcome to Dev Fusion!
-      </p>
-      <p className="text-black text-2xl font-semibold dark:text-white league-spartan text-center">
-        Your account has been successfully verified. We're excited to have you
-        join our open-source community. Let's create, collaborate, and innovate
-        together!
-      </p>
-      <p className="text-black text-2xl font-semibold dark:text-white league-spartan text-center">
-        Happy Coding!
-      </p>
-      <p className="text-black text-2xl font-semibold dark:text-white league-spartan text-center">
-        - The Dev Fusion Team
-      </p>
+      <div className="flex flex-col gap-10 text-black text-2xl dark:text-white text-center">
+        <p className="text-3xl font-semibold league-spartan">
+          Welcome to Dev Fusion!
+        </p>
+        <p className="league-spartan">
+          Your account has been successfully verified. We're excited to have you
+          join our open-source community. Let's create, collaborate, and
+          innovate together!
+        </p>
+        <p className="league-spartan">Happy Coding!</p>
+        <p className="league-spartan">- The Dev Fusion Team</p>
+      </div>
       <button
         onClick={goToLogin}
-        className="text-black font-semibold justify-center dark:text-white league-spartan text-xl"
+        className="font-semibold justify-center text-xl league-spartan"
       >
         Click here to sigin to your new account
       </button>
