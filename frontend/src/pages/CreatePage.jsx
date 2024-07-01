1350e6a9b00aa815c87ab3e1f624bb45dc6bb11d
James Salzer <salzer.james@yahoo.com>
2024-06-30 20:29:44 -0400

    - Added pathing for createpage and set up the email to send for forgot password

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -5,60 +5,58 @@ import NavBar from "../components/nav/NavBar";
 import BubblePanel from "../components/reusable/BubblePanel";
 
 const CreatePage = () => {
-	//TODO: Finish Creation Function
-	const doCreate = async (e) => {};
+  //TODO: Finish Creation Function
+  const doCreate = async (e) => {};
 
-	return (
-		<div>
-			<NavBar />
-			<div className="">
-				{/* Project Title */}
-				<Input
-					titleText="Project Title"
-					placeHolder="Enter Project Title"
-				></Input>
+  return (
+    <div>
+      <div className="">
+        {/* Project Title */}
+        <Input
+          titleText="Project Title"
+          placeHolder="Enter Project Title"
+        ></Input>
 
-				{/* TODO: Look into different format for date input */}
-				<div className="flex gap-2">
-					{/* Start Date */}
-					<Input
-						titleText="Start Date"
-						placeHolder="Enter the start date"
-					></Input>
+        {/* TODO: Look into different format for date input */}
+        <div className="flex gap-2">
+          {/* Start Date */}
+          <Input
+            titleText="Start Date"
+            placeHolder="Enter the start date"
+          ></Input>
 
-					{/* Deadline */}
-					<Input
-						titleText="Deadline"
-						placeHolder="Enter the deadline"
-					></Input>
-				</div>
+          {/* Deadline */}
+          <Input titleText="Deadline" placeHolder="Enter the deadline"></Input>
+        </div>
 
-				{/* Description */}
-				<Input
-					titleText="Description"
-					placeHolder="Tell us all about your project"
-				></Input>
+        {/* Description */}
+        <Input
+          titleText="Description"
+          placeHolder="Tell us all about your project"
+        ></Input>
 
-				{/* TODO: Technologies */}
-				<BubblePanel titleText="Technologies" techType></BubblePanel>
+        {/* TODO: Technologies */}
+        <BubblePanel titleText="Technologies" techType></BubblePanel>
 
-				{/* Expectations */}
-				<Input
-					titleText="Expectations"
-					placeHolder="What do you need from your roles?"
-				></Input>
+        {/* Expectations */}
+        <Input
+          titleText="Expectations"
+          placeHolder="What do you need from your roles?"
+        ></Input>
 
-				{/* TODO: Roles */}
-				<BubblePanel titleText="Roles" roleType></BubblePanel>
+        {/* TODO: Roles */}
+        <BubblePanel titleText="Roles" roleType></BubblePanel>
 
-				{/* TODO: Communication */}
-				<BubblePanel titleText="Communication" commsType></BubblePanel>
+        {/* TODO: Communication */}
+        <BubblePanel titleText="Communication" commsType></BubblePanel>
 
-				{/* Publish */}
-				<Button onClick={doCreate} large>
-					Publish
-				</Button>
-			</div>
-		</div>
-	);
+        {/* Publish */}
+        <Button onClick={doCreate} large>
+          Publish
+        </Button>
+      </div>
+    </div>
+  );
 };
+
+export default CreatePage;
--- frontend/src/pages/ResetPasswordEmailPage.jsx
+++ frontend/src/pages/ResetPasswordEmailPage.jsx
@@ -2,8 +2,34 @@ import { MdMailOutline } from "react-icons/md";
 import Input from "../components/reusable/Input";
 import Button from "../components/reusable/Button";
 import AuthPanel from "../components/reusable/AuthPanel";
+import { useState } from "react";
+import Axios from "axios";
 
 const ResetPasswordEmailPage = () => {
+  const [email, setEmail] = useState("");
+  const validEmail = new RegExp(
+    "^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"
+  );
+
+  const sendResetPasswordRequest = async () => {
+    if (validEmail.test(email)) {
+      const payload = { email: email };
+      try {
+        await Axios.post(
+          "http://localhost:5000/api/forgot_password/send",
+          payload,
+          { withCredentials: true }
+        );
+        console.log("DEBUG: Sent Payload");
+        console.log(payload);
+      } catch (err) {
+        console.log(`Error: ${err.message}`);
+      }
+    } else {
+      console.log("Email is not a valid");
+    }
+  };
+
   return (
     <AuthPanel width={480} minHeight={500}>
       <div className="flex flex-col justify-center items-center gap-2">
@@ -14,8 +40,15 @@ const ResetPasswordEmailPage = () => {
           Enter your email to reset your password
         </p>
       </div>
-      <Input titleText="Email" placeholder="Email" icon={<MdMailOutline />} />
-      <Button large>Submit</Button>
+      <Input
+        titleText="Email"
+        placeholder="Email"
+        icon={<MdMailOutline />}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Button large onClick={sendResetPasswordRequest}>
+        Submit
+      </Button>
     </AuthPanel>
   );
 };
