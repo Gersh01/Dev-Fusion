b321610b48e6efd11583b6960a756ac806306fcb
GoldenLin9 <goldenlin0909@gmail.com>
2024-07-02 22:51:19 -0400

    base cloud setup

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-	cloud_name: "duo5v3ro1",
-	api_key: "123456789012345",
-	api_secret: "abcdeghijklmnopqrstuvwxyz1234567890"
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const PORT = process.env.PORT || 5000;
@@ -87,13 +87,12 @@ app.listen(PORT, () => {
 });
 
 
-app.post("/api/upload", (req, res) => {
-
-
 
 const image = "https://images.unsplash.com/photo-1712237309240-aa707ccb516f?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
-cloudinary.uploader.upload(image)
-.then(result=> {
-	console.log(result.secure_url);
-})
+(async () => {
+
+    const result = await cloudinary.uploader.upload(image);
+    
+    console.log(result.secure_url);
+})();
