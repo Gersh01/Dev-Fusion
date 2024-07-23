4fb69217c03298c0d3aff44a274a2c1952f20e19
Xutao Gao <taogeeee@gmail.com>
2024-07-22 22:48:46 -0400

    Make API server HTTPS

--- frontend/src/utils/utility.js
+++ frontend/src/utils/utility.js
@@ -492,7 +492,7 @@ const getBubbleColor = (text) => {
 	return bubbleBgColors[hash % bubbleBgColors.length];
 };
 
-const apiDomain = "http://www.dev-fusion.com";
+const apiDomain = "https://www.dev-fusion.com";
 // const apiDomain = "http://localhost:5000";
 
 //Searches through list of tech and returns results based on partial match
